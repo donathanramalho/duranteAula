a721faa67cd77efebf35194c629318b23e60220e
Donathan Goncalves <donathanramalho@yahoo.com.br>
2026-04-10 14:39:08 -0300

    Working Project

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app = express();
 
 require('./startup/db')(); // Conectar banco
-// require('./startup/router')(app); // Rodar o router
+require('./startup/router')(app); // Rodar o router
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
